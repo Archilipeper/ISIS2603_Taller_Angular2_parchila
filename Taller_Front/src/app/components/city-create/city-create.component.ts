e63943282a410b9838bbed66bcc2122a3625bd24
Archilipeper <p.archila@uniandes.edu.co>
2026-05-15 21:05:10 -0500

    Parte C

--- Taller_Front/src/app/components/city-create/city-create.component.ts
+++ Taller_Front/src/app/components/city-create/city-create.component.ts
@@ -1,6 +1,11 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { CountryService } from '../../services/country.service';
+import { CityService } from '../../services/city.service';
+import { Country } from '../../models/country.model';
+import { City } from '../../models/city.model';
+import { CityListComponent } from '../city-list/city-list.component';
 
 /*
  * Implementar: HU-02 — Crear Ciudad
@@ -12,7 +17,38 @@ import { FormsModule } from '@angular/forms';
   imports: [CommonModule, FormsModule],
   templateUrl: './city-create.component.html'
 })
-export class CityCreateComponent {
+export class CityCreateComponent implements OnInit{
   @Output() cityCreated = new EventEmitter<void>();
   @Output() cancel = new EventEmitter<void>();
+
+  cityName: string = '';
+  selectedCountryId: number | null = null;
+  countries: Country[] = [];
+  countryService: CountryService;
+  cityService: CityService;
+
+  constructor(countryService: CountryService, cityService: CityService) {
+    this.countryService = countryService;
+    this.cityService = cityService;
+  }
+
+  ngOnInit(): void {
+      this.getCountries();
+  }
+
+  getCountries(): void {
+    this.countryService.getCountries().subscribe(countries => {
+      this.countries = countries;
+    });
+  }
+
+  OnSave(): void {
+    this.cityService.createCity(this.selectedCountryId!, { name: this.cityName }).subscribe(() => {
+      this.cityCreated.emit();
+    });
+  }
+
+  OnCancel(): void {
+    this.cancel.emit();
+  }
 }
